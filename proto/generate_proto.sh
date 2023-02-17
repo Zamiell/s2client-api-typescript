4cc6f63fbd566665a97b330674bc2660683d16d1
James <5511220+Zamiell@users.noreply.github.com>
2023-02-17 18:01:37 -0500

    chore: document script

--- proto/generate_proto.sh
+++ proto/generate_proto.sh
@@ -9,6 +9,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_ROOT="$DIR/.."
 cd "$REPO_ROOT"
 
-# "--ts_nocheck" is necessary to prevent errors with "override".
+# "ts_nocheck" adds a "// @ts-nocheck" line to the top of the file, which is necessary to prevent
+# errors with "override".
+# "eslint_disable" adds a "/* eslint-disable */" line to the top of the file.
 npx protoc --ts_opt "ts_nocheck" --ts_opt "eslint_disable" --ts_out "src/proto" --proto_path "proto" "proto/sc2api.proto" "proto/s2clientprotocol/*.proto"
 npx prettier --write "src/proto"
